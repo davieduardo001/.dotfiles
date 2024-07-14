d264cb213dd4df162f43608232c32f1becc98973
davi <davieduardocosta.miranda@gmail.com>
2024-07-14 13:57:28 -0300

    remove the vscode

--- bin/windowsrc.ps1
+++ bin/windowsrc.ps1
@@ -55,14 +55,6 @@ if (-not (Get-Command "choco" -ErrorAction SilentlyContinue)) {
     Write-Color "Chocolatey is already installed" $GREEN
 }
 
-# Install Visual Studio Code using winget
-Write-Color "Installing Visual Studio Code using winget..." $YELLOW
-if (-not (Get-Command "code" -ErrorAction SilentlyContinue)) {
-    winget install --id Microsoft.VisualStudioCode -e
-} else {
-    Write-Color "Visual Studio Code is already installed" $GREEN
-}
-
 # Install Code - OSS using winget
 Write-Color "Installing Code - OSS using winget..." $YELLOW
 if (-not (Get-Command "code-oss" -ErrorAction SilentlyContinue)) {
