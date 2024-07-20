9825109e7f8c2225f989a3abdecb59533b97253b
davi <ed.miranda@tutanota.com>
2024-07-20 12:50:24 +0000

    wsl installation

---
+++ bin/utils/wsl_install.py
@@ -0,0 +1,15 @@
+import utils.verifications as verify
+from utils.colors import colors
+
+# install on arch
+def install_sdkman():
+    verify.update_system('wsl')
+    verify.install_ansible('wsl')
+    verify.install_sdkman()
+    verify.install_git('wsl')
+    verify.intall_zip('wsl')
+    verify.install_nvm()
+    verify.install_ssh()
+    print(f'{colors.CYAN}* Play wsl playbook ***************{colors.RESET}\n')
+    verify.run_playbook('wsl')
+
